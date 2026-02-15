4aeafc5bcdaffb21c819977833d3d099c4d1e121
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-02-15 15:53:12 +0500

    feat(order): add order search and route loader support

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">SLICE CORE</Link>
+      <SearchOrder />
       <p>sheharyar</p>
     </header>
   );
